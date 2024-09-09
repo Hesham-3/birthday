96470d4d4dd98c6aad2ada941f5fc3ef56f9cea6
Hesham-3 <heshamshabka7@gmail.com>
2024-09-09 23:33:42 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    var celebrationDiv = document.getElementById('celebration');
+    celebrationDiv.style.display = 'block';
+    setTimeout(function() {
+        celebrationDiv.style.display = 'none';
+    }, 5000); // Hide the celebration after 5 seconds
+}
+
+ 
+ 
